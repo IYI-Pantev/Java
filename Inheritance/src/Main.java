ea5c4f72b3f041eb00698de8fbfa7c86f83f3d07
Nick Pantev <103816194+IYI-Pantev@users.noreply.github.com>
2025-07-09 15:13:49 +0300

    inheritance

---
+++ Inheritance/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Inheritance
+
+        Dog rocky = new Dog();
+        Cat ketty = new Cat();
+
+        rocky.eat();
+        rocky.speak();
+        ketty.eat();
+        ketty.speak();
+
+    }
+}
