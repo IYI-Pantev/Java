8edc86e2fba06911de2ee5b94c7a0da640449c26
Nick Pantev <103816194+IYI-Pantev@users.noreply.github.com>
2025-07-11 14:53:14 +0300

    wrapper class

---
+++ WrapperClasses/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Wrapper classes = Allow primitive values (int, char, double, boolean)
+        //                   to be used as objects. "Wrap them in an object"
+        //                   Generally, don't wrap primitives unless you need an object.
+        //                   Allows use of Collections Framework and static Utility Methods.
+
+
+        //Autoboxing - assigning primitives into an object
+        //int
+        Integer a = 123;
+        //double
+        Double b = 3.14;
+        //char
+        Character c = '$';
+        //bool
+        Boolean d = true;
+        // -
+        String e = "Hello";
+
+        //Unboxing
+        int x = a;
+
+    }
+
+}
